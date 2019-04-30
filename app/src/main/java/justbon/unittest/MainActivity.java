cee27d92ef2b635750df482b18d1f38ce26e3191
lifs <lifs419@126.com>
2019-04-30 16:58:46 +0800

    add a function

--- app/src/main/java/justbon/unittest/MainActivity.java
+++ app/src/main/java/justbon/unittest/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import justbon.module.Utils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        Utils.add(1,2);
     }
 
     @Override
---
+++ module/src/main/java/justbon/module/Utils.java
@@ -0,0 +1,8 @@
+package justbon.module;
+
+public class Utils {
+
+    public static int add(int i, int j) {
+        return i + j;
+    }
+}
